094e89d624ef8336c661eef51400325166959c6a
Darshik <131748885+Darshikchovatiya@users.noreply.github.com>
2024-08-23 18:10:44 +0530

    Watch mode understood and test other options

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Add_stu from './component/Add_stu/Add_stu'
 import View_stu from './component/View_stu/View_stu'
 import Header from './component/Header/Header'
@@ -8,14 +8,25 @@ import Edit_stu from './component/Edit_stu/Edit_stu'
 import View from './component/View/View'
 
 function App() {
+
+    const [data, setData] = useState('');
+    const [data1, setData1] = useState('');
+
     return (
         <>
-        <h1>Vite React</h1>
+        {/* <h1>Vite React</h1>
         <p>Hello coder First React TEST Case xyz</p>
         <p>Freedom is not given, it is taken</p>
         <img src="https://images.pexels.com/photos/6296908/pexels-photo-6296908.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" title='sky and mountain' />
+        <input type="text" placeholder='Enter name' name='name' id='nameId' value='darshik' /> */}
 
-        <input type="text" placeholder='Enter name' />
+        <h1>Test on change event</h1>
+        <input type="text" value={data} onChange={(e) => {setData(e.target.value + "test")}} />
+
+        <h1>Test Click event with button </h1>
+        <button onClick={()=> {setData1('updated data')}}>Updated data</button>
+        <h1>{data1}</h1>
+
             {/* <Header />
             <Routes>
                 <Route path='/' element={<Home />} />
--- src/App.test.js
+++ src/App.test.js
@@ -1,30 +1,99 @@
 import App from "./App";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 test('Always true test', () => {
     expect(true).toBe.true;
 })
 
-test('Heading should be Vite React', () => {
-    render(<App />);
-    const headingElement = screen.getByText('Vite React');
-    expect(headingElement).toBeInTheDocument();
-})
+// test.skip('Heading should be Vite React', () => {
+//     render(<App />);
+//     const headingElement = screen.getByText('Vite React');
+//     expect(headingElement).toBeInTheDocument();
+// })
 
-test('Test p tag value', () => {
-    render(<App />);
-    const text = screen.getByText(/First React Test Case/i);
-    const text2 = screen.getByText('Freedom is not given, it is taken');
-    const imgtitle = screen.getByTitle('sky and mountain');
-    expect(text).toBeInTheDocument();
-    expect(text2).toBeInTheDocument();
-    expect(imgtitle).toBeInTheDocument();
-})
+// test('Test p tag value', () => {
+//     render(<App />);
+//     const text = screen.getByText(/First React Test Case/i);
+//     const text2 = screen.getByText('Freedom is not given, it is taken');
+//     const imgtitle = screen.getByTitle('sky and mountain');
+//     expect(text).toBeInTheDocument();
+//     expect(text2).toBeInTheDocument();
+//     expect(imgtitle).toBeInTheDocument();
+// })
 
-test('Testing input box', () => {
+// test('Testing input box', () => {
+//     render(<App />);
+//     const checkInput = screen.getByRole('textbox');
+//     const checkInputplaceholder = screen.getByPlaceholderText('Enter name');
+//     expect(checkInput).toBeInTheDocument();
+//     expect(checkInputplaceholder).toBeInTheDocument();
+//     expect(checkInput).toHaveAttribute('name', 'name');
+//     expect(checkInput).toHaveAttribute('id', 'nameId');
+//     expect(checkInput).toHaveAttribute('type', 'text');
+//     expect(checkInput).toHaveAttribute('value', 'darshik');
+// })
+
+// describe.skip('Ui test case group', () => {
+
+//     test('test case 1', () => {
+//         render(<App />);
+//         const checkInput = screen.getByRole('textbox');
+//         expect(checkInput).toHaveAttribute('name', 'name');
+//     })
+
+//     test('test case 2', () => {
+//         render(<App />);
+//         const checkInput = screen.getByRole('textbox');
+//         expect(checkInput).toHaveAttribute('type', 'text');
+//     })
+
+//     test('test case 3', () => {
+//         render(<App />);
+//         const checkInput = screen.getByRole('textbox');
+//         expect(checkInput).toHaveAttribute('name', 'name');
+//     })
+// })
+
+// describe('Api test case group', () => {
+
+//     test('APi test case 1', () => {
+//         render(<App />);
+//         const checkInput = screen.getByRole('textbox');
+//         expect(checkInput).toHaveAttribute('name', 'name');
+//     })
+
+//     test('Api test case 2', () => {
+//         render(<App />);
+//         const checkInput = screen.getByRole('textbox');
+//         expect(checkInput).toHaveAttribute('type', 'text');
+//     })
+
+//     test('Api test case 3', () => {
+//         render(<App />);
+//         const checkInput = screen.getByRole('textbox');
+//         expect(checkInput).toHaveAttribute('name', 'name');
+//     })
+
+//     describe('inner describe test case group', () => {
+//         test('inner describe test case 1', () => {
+//             render(<App />);
+//             const checkInput = screen.getByRole('textbox');
+//             expect(checkInput).toHaveAttribute('id', 'nameId');
+//         })
+//     })
+// })
+
+
+
+test('on change event testing', ()=>{
     render(<App />);
     const checkInput = screen.getByRole('textbox');
-    const checkInputplaceholder = screen.getByPlaceholderText('Enter name');
-    expect(checkInput).toBeInTheDocument();
-    expect(checkInputplaceholder).toBeInTheDocument();
+    fireEvent.change(checkInput,{target:{value:'dev'}});
+    expect(checkInput.value).toBe('devtest');
 })
+
+// test('click event testing', () => {
+//     render(<App />);
+//     const checkBtn = screen.getByRole('button');
+//     fireEvent.click(checkBtn,)
+// })
